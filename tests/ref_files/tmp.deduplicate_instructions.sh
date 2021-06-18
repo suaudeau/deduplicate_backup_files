8c8fa51f7e35d219a49880b4454ea9a477ada6c5
suaudeau <herve.suaudeau@parisdescartes.fr>
2021-06-18 11:52:25 +0200

    V1.8: Preserve dates, ownership and rights of deduplicated files

--- tests/ref_files/tmp.deduplicate_instructions.sh
+++ tests/ref_files/tmp.deduplicate_instructions.sh
@@ -1,32 +1,40 @@
 if [[ -f "./file10k_double.txt" ]]; then
-  rm -f "./dup1/file10k_double.txt"
+  mv "./dup1/file10k_double.txt"  "./dup1/file10k_double.txt.moved"
   cp -al "./file10k_double.txt" "./dup1/file10k_double.txt"
   chmod 712 "./dup1/file10k_double.txt"
   chown USER:USER "./dup1/file10k_double.txt"
+  touch "./dup1/file10k_double.txt" -r "./dup1/file10k_double.txt.moved"
+  rm -f "./dup1/file10k_double.txt.moved"
 fi
 
 
 if [[ -f "./file1k_double.txt" ]]; then
-  rm -f "./dup1/file1k_double.txt"
+  mv "./dup1/file1k_double.txt"  "./dup1/file1k_double.txt.moved"
   cp -al "./file1k_double.txt" "./dup1/file1k_double.txt"
   chmod 712 "./dup1/file1k_double.txt"
   chown USER:USER "./dup1/file1k_double.txt"
+  touch "./dup1/file1k_double.txt" -r "./dup1/file1k_double.txt.moved"
+  rm -f "./dup1/file1k_double.txt.moved"
 fi
 
 
 if [[ -f "./file100k_triple.txt" ]]; then
-  rm -f "./dup1/file100k_triple.txt"
+  mv "./dup1/file100k_triple.txt"  "./dup1/file100k_triple.txt.moved"
   cp -al "./file100k_triple.txt" "./dup1/file100k_triple.txt"
   chmod 712 "./dup1/file100k_triple.txt"
   chown USER:USER "./dup1/file100k_triple.txt"
+  touch "./dup1/file100k_triple.txt" -r "./dup1/file100k_triple.txt.moved"
+  rm -f "./dup1/file100k_triple.txt.moved"
 fi
 
 
 if [[ -f "./file100k_triple.txt" ]]; then
-  rm -f "./dup1/dup2/file100k_triple.txt"
+  mv "./dup1/dup2/file100k_triple.txt"  "./dup1/dup2/file100k_triple.txt.moved"
   cp -al "./file100k_triple.txt" "./dup1/dup2/file100k_triple.txt"
   chmod 713 "./dup1/dup2/file100k_triple.txt"
   chown USER:adm "./dup1/dup2/file100k_triple.txt"
+  touch "./dup1/dup2/file100k_triple.txt" -r "./dup1/dup2/file100k_triple.txt.moved"
+  rm -f "./dup1/dup2/file100k_triple.txt.moved"
 fi
 
 
